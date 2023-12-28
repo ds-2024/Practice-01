6d2510a5d89051bd5d71b43910420a87d8d89033
ds-2024 <yoonds714@nate.com>
2023-12-28 09:49:07 +0900

    추가

---
+++ src/com/javaex/practice/Ex36.java
@@ -0,0 +1,32 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex36 {
+
+	public static void main(String[] args) {
+		
+		// 동전갯수 입력받아 총금액 출력
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("500원 개수: ");
+		int a = sc.nextInt();
+		
+		System.out.print("100원 개수: ");
+		int b = sc.nextInt();
+		
+		System.out.print("50원 개수: ");
+		int c = sc.nextInt();
+		
+		System.out.print("10원 개수: ");
+		int d = sc.nextInt();
+		
+		System.out.print("동전의 총합은 " + (500*a+100*b+50*c+10*d) + " 원 입니다.");
+		
+		
+		sc.close();
+
+	}
+
+}
